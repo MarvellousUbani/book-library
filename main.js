9d46725d2f80e6f1a4880c1188d271e69e288544
dflexcee <marvellousubani@gmail.com>
2020-05-20 17:59:52 +0100

    completed render function to display books on dom

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let bookCard = document.getElementById('book-card');
+
+
 let myLibrary = [];
 
 function Book(author, title, pages, read = false) {
@@ -13,10 +16,10 @@ function addBookToLibrary(author, title, pages, read) {
 
 function render() {
     for (let book of myLibrary) {
-        console.log(`Author: ${book.author}
-        Title: ${book.title}
-        Pages: ${book.pages}
-        Read: ${book.read}`);
+      const bookItem = document.createTextNode(`Author: ${book.author} Title: ${book.title} Pages: ${book.pages} Read: ${book.read}`)
+      let bookElem =  document.createElement('li');
+      bookElem.appendChild(bookItem)
+      bookCard.appendChild(bookElem);
     }
 }
 
@@ -24,8 +27,9 @@ function render() {
 let test = addBookToLibrary('Mike', 'Cooking book', 55);
 let test1 = addBookToLibrary('Roodz', 'notebook', 15, true);
 let test2 = addBookToLibrary('Tolkien', 'The Hobbit', 158, false);
+let test3 = addBookToLibrary('Ultra', 'The Hobbit', 158, false);
 // console.log(myLibrary);
 render();
 
-// let bookCard = document.getElementById('book-card');
+
 // bookCard.innerHTML = render();
